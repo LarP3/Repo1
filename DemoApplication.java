7ddc9868164f7c54d4d2f5c83a7d651ba02e8cdc
LarP3 <91810734+LarP3@users.noreply.github.com>
2022-02-28 10:20:26 -0500

    Update DemoApplication.java

--- DemoApplication.java
+++ DemoApplication.java
@@ -18,7 +18,7 @@ SpringApplication.run(DemoApplication.class, args);
 }
 
 @GetMapping("/hello")
-public String hello(@RequestParam(value = "Larry", defaultValue = "World") String name) {
+public String hello(@RequestParam(value = "name", defaultValue = "Paul") String name) {
 return String.format("Hello %s!", name);
 }
 }
